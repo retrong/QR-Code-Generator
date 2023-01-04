e819c9aa0440d92679cf62d7b871e10a0ef67a0b
retrong <eguobabarth@gmail.com>
2023-01-04 02:11:32 +0100

    modified existing html and js page

---
+++ main.js
@@ -0,0 +1,9 @@
+var qrcode = undefined;
+function generateQRCode(value) {
+    if (qrcode === undefined) {
+        qrcode = new QRCode(document.getElementById('qrcode'), value);
+    } else {
+        qrcode.clear();
+        qrcode.makeCode(value);
+    }
+}
